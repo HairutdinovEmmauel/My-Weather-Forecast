3496d61d201023ff97940234cd39bbeedfd545b0
Эммануил <52475472+HairutdinovEmmauel@users.noreply.github.com>
2019-07-14 08:39:49 +0300

    Зделали простой прогноз погоды

--- Новая папка/js/script.js
+++ Новая папка/js/script.js
@@ -1,280 +1,168 @@
 window.onload = function () {
-	document.write("<h1>Hello</h1>")
-	var options = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0
-	  };
-	  
-	  function success(pos) {
-		var crd = pos.coords;
-	  
-		console.log('Your current position is:');
-		console.log(`Latitude : ${crd.latitude}`);
-		console.log(`Longitude: ${crd.longitude}`);
-		console.log(`More or less ${crd.accuracy} meters.`);
-	  };
-	  
-	  function error(err) {
-		console.warn(`ERROR(${err.code}): ${err.message}`);
-	  };
-	  
-	  navigator.geolocation.getCurrentPosition(success, error, options);
+	var button = document.getElementById('Geolocation');
+	iMap();
 
-
-
-
-
-	/*var options = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0
-	  };
-
-	  var lat = position.coords.latitude;
-	  var lng = position.coords.longitude;
-	  
-	function success(position) {
-		var lati = lat;
-		var long = lng;
+	function iMap() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 50.900, lng: 31.320},
+			zoom: 5
+		});
 	};
-	  
-	function error(err) {
-		console.warn(`ERROR(${err.code}): ${err.message}`);
-	};
-	  
-	navigator.geolocation.getCurrentPosition(success, error, options);
 
-	object = {
-		center: {lat: lat, lng: lng},
-		zoom: 8
-	}
-
-	function initMap() {
-		var map = new google.maps.Map(document.getElementById('map'), object)};
-
-	
-	// 	var map = new google.maps.Map(document.getElementById('map'), {
-	// 		center: {lat: 47.900, lng: 36.320},
-	// 		zoom: 12
-	// 	});
-	// };
-
-	// options = {
-	// 	lat: position.coords.latitude,
-	// 	lng: position.coords.longitude
-	// }
-
-	// navigator.geolocation.getCurrentPosition(
-	// 	function initMap() {
-	// 	var map = new google.maps.Map(document.getElementById('map'), {
-	// 		center: {lat: options[0], lng: options[1]},
-	// 		zoom: 12
-	// 	});
-	// }); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 	//------------------------------------------------- НАЧАЛО -----------------------------------------------------
-	/*
-		JavaScript позволяет реализовать поставленную задачу посредством использования Geolocation API. В вашем 
-		распоряжении будет небольшой набор функций с помощью которых возможно определение позиции устройства по:
-
-		GPS – для мобильных устройств, наиболее точный метод (погрешность в пределе 10 метров).
-		WiFi – доступен для всех подключённых устройств. Довольно точен.
-		Геолокация по IP – привязана к региону. Часто даёт большую погрешность, в зависимости от расположения 
-		оборудования провайдера.
-		При запросе данных, браузер попытается извлечь информацию всеми тремя способами. Позиция по WiFi определяется 
-		быстрее по сравнению с GPS и наиболее точна по сравнению с геолокацией по IP.
-
-		Работа с Geolocation API
-		Geolocation API довольно-таки неплохо поддерживается браузерами, но всё равно было бы неплохо проверить 
-		доступность объекта Window.navigator.
-
-			if (navigator.geolocation) {
-			// Геолокация доступна
-			}
-			else {
-			// Геолокация не доступна
-			}
-	*/
-
-	//--------------------------------------------------------------------------------------------------------------
-	/* 
-			В объекте navigator.geolocation доступны следующие методы:
-
-		Geolocation.getCurrentPosition() – Определение текущей позиции устройства.
-		Geolocation.watchPosition() – Отслеживание изменения позиции устройства и вызов функции обратного вызова.
-		Geolocation.clearWatch() – Удаление обработчика метода watchPosition.
-		Методы getCurrentPosition() и watchPosition() используются для схожих целей. Оба метода работают асинхронно, 
-		пытаясь извлечь позицию устройства.
-
-
-
-
-
-
+	button.addEventListener('click', weatherForecast);
+	function weatherForecast () {
 
 		navigator.geolocation.getCurrentPosition(
+			// Функция обратного вызова при успешном извлечении локации
+			function (position) {
+				position = {
+					coords: {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude
+					}
+				}
 
-    // Функция обратного вызова при успешном извлечении локации
-    function(position) {
+				// Создание своей Функции
+				console.log( "Последний раз вас нашли" + '\n' + position.coords.lat + '\n' +  position.coords.lng);
+					map();
 
-	/*
-        В объекте position изложена подробная информация
-        о позиции устройства:
+					function map () {
+						var map = new google.maps.Map(document.getElementById('map'), {
+							center: {lat: position.coords.lat, lng: position.coords.lng},
+							zoom: 14
+						});
+						
+					};
 
-        position = {
-            coords: {
-                latitude - Широта.
-                longitude - Долгота.
-                altitude - Высота в метрах над уровнем моря.
-                accuracy - Погрешность в метрах.
-                altitudeAccuracy - Погрешность высоты над уровнем моря в метрах.
-                heading - Направление устройства по отношению к северу.
-                speed - Скорость в метрах в секунду.
-            }
-            timestamp - Время извлечения информации.
-        }
-        
+				// 1. Создаём новый объект XMLHttpRequest
+				var xhr = new XMLHttpRequest();
+				var URL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + String(position.coords.lat) + '&'  + 'lon=' + String(position.coords.lng) + '&APPID=3f8430d4d06ce3e26656fc62b981aaf0';
+				// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+				xhr.open('GET', URL, true);
+				// Эта конфигуратция не успешная по причине ошибки сервера xhr.open('GET', 'https://api.darksky.net/forecast/97c7f099b8b85f8d0eca0dcf50ec81fd/' + String(position.coords.lat) + ',' + String(position.coords.lng), true);
 
-    },
-	
-		// Функция обратного вызова при неуспешном извлечении локации
-		function(error){
-	
+				// 3. Отсылаем запрос 
+				xhr.send(); 
+
+				
+
+				xhr.onreadystatechange = function () {
+					
+					if (xhr.readyState !== 4) {
+						return
+					}
+					console.log('end'); // Оброботка обьекта завершена
+					
+					if (xhr.status === 200) {
+						// вывести результат
+						// console.log('result', JSON.parse( xhr.responseText )); // responseText -- текст ответа.
+						var GetWeath = JSON.parse( xhr.responseText );
+						Result();
+						
+						function Result () {
+							document.querySelector('.text').innerHTML = "";
+
+							var result = document.querySelector('.text');
+
+							var tegH3 = document.createElement('h3');
+							var textHeadline = document.createTextNode('Прогноз погоды на Сегодне');
+							
+							result.appendChild(tegH3);
+							tegH3.appendChild(textHeadline);
+							tegH3.style.textAlign = 'center';
+
+							var weatherIcon = document.createElement('p');
+							var img = document.createElement('img');
+							
+							result.appendChild(weatherIcon);
+							weatherIcon.appendChild(img);
+							
+							img.src = 'https://openweathermap.org/img/w/' + GetWeath.weather[0].icon + '.png';
+							img.style.width = '50px';
+							img.style.height = '50px';
+
+							var min = + Math.floor( GetWeath.main.temp_min - 273 );
+							var temp_min = document.createElement('p');
+							var temp_info = document.createTextNode('Минимальная температура: ' + min);
+
+							result.appendChild(temp_min);
+							temp_min.appendChild(temp_info);
+							
+							var max = Math.floor( GetWeath.main.temp_max - 273 );
+							var temp_max = document.createElement('p');
+							var temp_maxInfo = document.createTextNode('Максимальная температура: ' + max);
+								
+							result.appendChild(temp_max);
+							temp_max.appendChild(temp_maxInfo)
+								
+							var humidity = Math.floor( GetWeath.main.humidity);
+							var Humidity = document.createElement('p');
+							var HumidityInfo = document.createTextNode('Влажность: ' + humidity + '%');
+								
+							result.appendChild(Humidity);
+							Humidity.appendChild(HumidityInfo);
+							
+							var pressure = Math.floor( GetWeath.main.pressure);
+							var Pressure = document.createElement('p');
+							var PressureInfo = document.createTextNode('Давление: ' + pressure);
+								
+							result.appendChild(Pressure);
+							Pressure.appendChild(PressureInfo)
+						}                                                                                                                                                           
+						
+						// иконка для погоды 02d.png
+					} else {
+						// обработать ошибку
+						console.log('error', xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+					}
+					
+				};
+
+				/*
+				В объекте position изложена подробная информация
+				о позиции устройства:
+
+				position = {
+					coords: {
+						latitude - Широта.
+						longitude - Долгота.
+						altitude - Высота в метрах над уровнем моря.
+						accuracy - Погрешность в метрах.
+						altitudeAccuracy - Погрешность высоты над уровнем моря в метрах.
+						heading - Направление устройства по отношению к северу.
+						speed - Скорость в метрах в секунду.
+					}
+					timestamp - Время извлечения информации.
+				}
+				*/
+			},
+
+			// Функция обратного вызова при неуспешном извлечении локации
+			function(error){
+				error = {
+					code: 'Доступ к месту положения не доступен.',
+					message: 'Вы не разрешили доступ к вашему месту расположения.'
+				}
+				document.querySelector('.text').innerHTML = error.code + '<br>' + error.message;
+
+				
+			/*    
 			
-			При неудаче, будет доступен объект error:
-	
-			error = {
-				code - Тип ошибки
-						1 - PERMISSION_DENIED
-						2 - POSITION_UNAVAILABLE
-						3 - TIMEOUT
-	
-				message - Детальная информация.
+				При неудаче, будет доступен объект error:
+
+				error = {
+					code - Тип ошибки
+							1 - PERMISSION_DENIED
+							2 - POSITION_UNAVAILABLE
+							3 - TIMEOUT
+
+					message - Детальная информация.
+				}
+				
+			*/
+
 			}
-			
-	
-		}
-	);
-	//-------------------------------------------------------------------------------------------------------------
-	*/
-	/*
-		Браузер позаботится за отображение окна подтверждения, но есть и другой способ. Альтернативный способ может 
-		пригодится в случае отказа пользователя, которому в дальнейшем браузер не будет предлагать предоставить 
-		доступ.
-		
-		Безопасные хосты
-		Ещё одним препятствием к использованию данного API является обязательная поддержка HTTPS. Согласно новым 
-		правилам безопасности, Google Chrome не позволит ненадёжным хостам запускать Geolocation API. Так что вам 
-		придётся установить SSL сертификат на ваш домен.
-		
-		Подробнее об этом можно найти тут.
 
-
-
-
-
-
-
-
-
-
-
-
-
-	*/
-	//---------------------------------------------------------------------------------------------------
-	/*
-	
-	извлекаем и отображаем на карте местоположение устройства пользователя.view sourceprint?
-	
-		findMeButton.on('click', function(){
-
-			navigator.geolocation.getCurrentPosition(function(position) {
-
-			// Текущие координаты.
-			var lat = position.coords.latitude;
-			var lng = position.coords.longitude;
-
-			// Отрисовка карты.
-			var map = new GMaps({
-				el: '#map',
-				lat: lat,
-				lng: lng
-			});
-
-			map.addMarker({
-				lat: lat,
-				lng: lng
-			});
-
-    });
-
-});
-		
-	
-*/
-
-	// var map;
-	// function initMap() {
-	// 	map = new google.maps.Map(document.getElementById('map'), {
-	// 		center: {lat: -34.397, lng: 150.644},
-	// 		zoom: 8
-	// 	});
-	// };
-
-	// var latit = position.coords.latitude;
-	// var longi = position.coords.longitude;
-	
-	// navigator.geolocation.getCurrentPosition(
-	// 	function () {
-	// 		alert('Последний раз вас засекали здесь: ' +
-	// 			latit + ", " + longi );
-	// 	}
-	// );
-
-	// var buttonGeol = document.getElementById('Geolocation');
-
-	// buttonGeol.addEventListener('mousedown', FunGeolocation);
-	// function FunGeolocation () {
-	// 	map, {
-	// 		center: {lat: -34.397, lng: 150.644},
-	// 		zoom: 18
-	// 	};
-	// };
-	// }; 
-};
-/*
-В объекте navigator.geolocation доступны следующие методы:
-
-Geolocation.getCurrentPosition() – Определение текущей позиции устройства.
-Geolocation.watchPosition() – Отслеживание изменения позиции устройства и вызов функции обратного вызова.
-Geolocation.clearWatch() – Удаление обработчика метода watchPosition.
-Методы getCurrentPosition() и watchPosition() используются для схожих целей. Оба метода работают асинхронно, 
-пытаясь извлечь позицию устройства.
-
-*/
+	)};
+}; 
+	 
